4f175ac707a454d4d487be060dc259e55065de1f
ISNewton <ashrfalhag.11@gmail.com>
2023-10-13 17:16:42 +0200

    Fix cors

--- src/components/auth/RegisterForm.tsx
+++ src/components/auth/RegisterForm.tsx
@@ -2,10 +2,33 @@ import { Field, Form, Formik } from "formik";
 import { z } from "zod";
 import { toFormikValidationSchema } from "zod-formik-adapter";
 import TextInput from "../base/TextInput";
-import { useQuery } from "react-query";
+import { useMutation, useQuery } from "react-query";
 import client from "@/libs/client";
+import { useState } from "react";
+import axios, { AxiosError } from "axios";
 
 export default function RegisterForm() {
+    const [formErrors, setFormErrors] = useState<object[]>([]);
+    const mutation = useMutation({
+        mutationFn: async (data) => {
+            try {
+                const res = await client.post("auth/signup");
+                console.log('success')
+                return res
+            }
+            catch (e) {
+                const errors = e as AxiosError
+                // console.log('error' , errors.response)
+                setFormErrors((errors.response?.data?.message) as [])
+                console.log(errors.response?.data?.message)
+
+            }
+            // console.log(data);
+
+        },
+
+    });
+
     const initValues = {
         name: "",
         email: "",
@@ -36,77 +59,86 @@ export default function RegisterForm() {
     };
 
     async function register(data: RegisterFormType) {
-        const res = await client.post("signup", data);
-        console.log(res);
+        await mutation.mutateAsync(data);
+
+        console.log('registering...')
     }
 
     return (
+
         <>
+            {/* { formErrors && formErrors.map(e => { */}
+            {/*     return <p>{[e]}</p> */}
+
+            {/* })} */}
             <Formik
                 initialValues={initValues}
                 validationSchema={toFormikValidationSchema(registerFormSchema)}
                 onSubmit={register}
             >
                 {(form) => (
-                    <Form>
-                        <TextInput
-                            name="name"
-                            className="w-full p-2 bg-gray-900 rounded-md text-white  border border-gray-700 focus:border-blue-700"
-                            placeholder="Full name"
-                            type="text"
-                            onChange={form.handleChange}
-                            value={form.values.name}
-                            error={form.errors.name}
-                            touched={form.touched.name}
-                        />
+                <Form>
+                    <TextInput
+                        name="name"
+                        className="w-full p-2 bg-gray-900 rounded-md text-white  border border-gray-700 focus:border-blue-700"
+                        placeholder="Full name"
+                        type="text"
+                        onChange={form.handleChange}
+                        value={form.values.name}
+                        error={form.errors.name}
+                        touched={form.touched.name}
+                    />
 
-                        <TextInput
-                            name="email"
-                            className="w-full p-2 bg-gray-900 rounded-md text-white  border border-gray-700 focus:border-blue-700"
-                            placeholder="Email"
-                            type="email"
-                            onChange={form.handleChange}
-                            value={form.values.email}
-                            error={form.errors.email}
-                            touched={form.touched.email}
-                        />
+                    <TextInput
+                        name="email"
+                        className="w-full p-2 bg-gray-900 rounded-md text-white  border border-gray-700 focus:border-blue-700"
+                        placeholder="Email"
+                        type="email"
+                        onChange={form.handleChange}
+                        value={form.values.email}
+                        error={form.errors.email}
+                        touched={form.touched.email}
+                    />
 
-                        <TextInput
-                            name="username"
-                            className="w-full p-2 bg-gray-900 rounded-md text-white  border border-gray-700 focus:border-blue-700"
-                            placeholder="Twitter Username"
-                            type="text"
-                            onChange={form.handleChange}
-                            value={form.values.username}
-                            error={form.errors.username}
-                            touched={form.touched.username}
-                        />
+                    <TextInput
+                        name="username"
+                        className="w-full p-2 bg-gray-900 rounded-md text-white  border border-gray-700 focus:border-blue-700"
+                        placeholder="Twitter Username"
+                        type="text"
+                        onChange={form.handleChange}
+                        value={form.values.username}
+                        error={form.errors.username}
+                        touched={form.touched.username}
+                    />
 
-                        <TextInput
-                            name="password"
-                            className="w-full p-2 bg-gray-900 rounded-md text-white  border border-gray-700 focus:border-blue-700"
-                            placeholder="Password"
-                            type="password"
-                            onChange={form.handleChange}
-                            value={form.values.password}
-                            error={form.errors.password}
-                            touched={form.touched.password}
-                        />
+                    <TextInput
+                        name="password"
+                        className="w-full p-2 bg-gray-900 rounded-md text-white  border border-gray-700 focus:border-blue-700"
+                        placeholder="Password"
+                        type="password"
+                        onChange={form.handleChange}
+                        value={form.values.password}
+                        error={form.errors.password}
+                        touched={form.touched.password}
+                    />
 
-                        <TextInput
-                            name="passwordConfirmation"
-                            className="w-full p-2 bg-gray-900 rounded-md text-white  border border-gray-700 focus:border-blue-700"
-                            placeholder="Confirm password"
-                            type="password"
-                            onChange={form.handleChange}
-                            value={form.values.passwordConfirmation}
-                            error={form.errors.passwordConfirmation}
-                            touched={form.touched.passwordConfirmation}
-                        />
-                        <button type="submit" className="btn btn-light w-full rounded-full mt-4">
-                            Register
-                        </button>
-                    </Form>
+                    <TextInput
+                        name="passwordConfirmation"
+                        className="w-full p-2 bg-gray-900 rounded-md text-white  border border-gray-700 focus:border-blue-700"
+                        placeholder="Confirm password"
+                        type="password"
+                        onChange={form.handleChange}
+                        value={form.values.passwordConfirmation}
+                        error={form.errors.passwordConfirmation}
+                        touched={form.touched.passwordConfirmation}
+                    />
+                    <button
+                        type="submit"
+                        className="btn btn-light w-full rounded-full mt-4"
+                    >
+                        Register
+                    </button>
+                </Form>
                 )}
             </Formik>
         </>
--- src/libs/client.ts
+++ src/libs/client.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
 
 axios.defaults.baseURL = 'http://localhost:3001'
+axios.defaults.headers['Content-Type'] = 'application/json'
 
 export default axios
